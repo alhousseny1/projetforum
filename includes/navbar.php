48785ac6af9538a169909a3b2930ac4ebcbd9b43
alhousseny1 <senydiaby93@gmail.com>
2022-06-06 16:51:05 +0200

    logo

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Forum</a>
+    <a class="navbar-brand" href="index.php"><img src="logo.png" alt="logo" width="80" height="80"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
